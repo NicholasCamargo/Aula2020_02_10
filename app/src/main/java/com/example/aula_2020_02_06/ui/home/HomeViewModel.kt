4cbb923e753526988ac98561d98ed30690b7309a
NicholasCamargo <nicholas.camargo@al.infnet.edu.br>
2020-02-10 11:09:17 -0300

    Primeiro Commit com os arquivos base

---
+++ app/src/main/java/com/example/aula_2020_02_06/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.aula_2020_02_06.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
